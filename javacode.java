dce06d2c7dfdeeba4da7b1a7898054b12f729748
Muthurajjjj <153120343+Muthurajjjj@users.noreply.github.com>
2023-12-16 11:22:38 +0530

    Update javacode.java

--- javacode.java
+++ javacode.java
@@ -3,7 +3,7 @@ public class SumofOddNUMBER {
 	public static void main(String[] args) {
 		int c=0;
 		for(int i=1;i<=10;i++) {
-			if(i%2==0) {
+			if(i%2==1) {
 				c=c+i;
 			}
 		}System.out.println(c);
